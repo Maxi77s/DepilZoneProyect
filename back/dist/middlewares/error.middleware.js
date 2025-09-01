bb01cd10c7d46b015c465b790e52a38161bd47b1
Maximiliano <Fnievas68@gmail.com>
2025-09-01 17:53:09 -0300

    act

---
+++ back/dist/middlewares/error.middleware.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.errorHandler = void 0;
+const errorHandler = (err, _req, res, _next) => {
+    console.error(err);
+    res.status(err.status || 500).json({
+        error: err.message || "Error interno del servidor",
+    });
+};
+exports.errorHandler = errorHandler;
+//# sourceMappingURL=error.middleware.js.map
